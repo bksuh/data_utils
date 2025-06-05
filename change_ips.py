aecc6637a33107e8d5da32f5e942013d3d68e64f
Bokeon Suh <162313703+bksuh@users.noreply.github.com>
2025-06-05 23:41:06 +0900

    modified

--- change_ips.py
+++ change_ips.py
@@ -1,5 +1,5 @@
 import json
-import sys
+import argparse
 
 def update_json_fields(path, t_ip, l_ip):
     with open(path, 'r') as f:
@@ -18,22 +18,15 @@ def update_json_fields(path, t_ip, l_ip):
         json.dump(data, f, indent=2)
 
 if __name__ == "__main__":
-    # Check if arguments are provided
-    if len(sys.argv) != 3:
-        print("Usage: python3 change_ips.py t_ip=<target_ip> l_ip=<lidar_ip>")
-        sys.exit(1)
-
-    # Parse arguments
-    try:
-        t_ip = sys.argv[1].split("=")[1]
-        l_ip = sys.argv[2].split("=")[1]
-    except IndexError:
-        print("Error: Arguments must be in the format t_ip=<target_ip> l_ip=<lidar_ip>")
-        sys.exit(1)
+    # Use argparse for argument parsing
+    parser = argparse.ArgumentParser(description="Update IP addresses in a JSON configuration file.")
+    parser.add_argument("--t_ip", required=True, help="Target IP address")
+    parser.add_argument("--l_ip", required=True, help="Lidar IP address")
+    args = parser.parse_args()
 
     # Path to the JSON file
     json_path = "/root/ros2_ws/install/livox_ros_driver2/share/livox_ros_driver2/config/MID360_config.json"
 
     # Update JSON fields
-    update_json_fields(json_path, t_ip, l_ip)
-    print(f"Updated JSON file at {json_path} with t_ip={t_ip} and l_ip={l_ip}")
+    update_json_fields(json_path, args.t_ip, args.l_ip)
+    print(f"Updated JSON file at {json_path} with t_ip={args.t_ip} and l_ip={args.l_ip}")
